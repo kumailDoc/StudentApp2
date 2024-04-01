c0707f51a0790cf40b11103e88f307616e42ec0f
Kumail Doctor <dkumail@my.centennialcollege.ca>
2024-04-01 15:14:22 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import StudentForm from 'studentapp1/StudentForm';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>StudentApp2</h1>
+      <StudentForm />
+    </div>
+  );
+}
+
+export default App;
